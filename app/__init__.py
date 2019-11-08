6b9331a75d12b8c86b11ac37cde2a2dfb4202ff3
Grant <gth2103@columbia.edu>
2019-11-08 10:42:00 -0500

    added user register login and database

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,20 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/almynak.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view == "login"
+
+from app import views, models
--- run.py
+++ run.py
@@ -1,4 +1,17 @@
-from app import app, views
+from app import app, db
+from app.models import *
+
+admin = User(username='admin', email='gth2103@columbia.edu', password_hash=generate_password_hash('admin'))
+guest = User(username='guest', email='guest@columbia.edu', password_hash=generate_password_hash('guest'))
+grant =  User(username='grant', email='grant@columbia.edu', password_hash=generate_password_hash('grant'))
+john =  User(username='john', email='john@columbia.edu', password_hash=generate_password_hash('john'))
+jane =  User(username='jane', email='jane@columbia.edu', password_hash=generate_password_hash('jane'))
+
+db.session.add(guest)
+db.session.add(grant)
+db.session.add(john)
+db.session.add(jane)
+db.session.commit()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug = True)
